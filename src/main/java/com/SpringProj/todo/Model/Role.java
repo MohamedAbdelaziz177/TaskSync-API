882a4bdca09c6b9d9696f33c5571eb291825cae8
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-03-25 04:46:07 +0200

    Adding role entity and configuring the relationship with user

--- src/main/java/com/SpringProj/todo/Model/Role.java
+++ src/main/java/com/SpringProj/todo/Model/Role.java
@@ -1,4 +1,23 @@
 package com.SpringProj.todo.Model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name = "role")
+@Data
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "role cannot be null")
+    private String role;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
 }

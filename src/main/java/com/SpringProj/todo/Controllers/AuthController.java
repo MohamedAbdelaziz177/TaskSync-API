b373a4498967ef78ec7912e2ea8f2ac877e37d10
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-03-27 05:41:18 +0200

    updating auth controller

---
+++ src/main/java/com/SpringProj/todo/Controllers/AuthController.java
@@ -0,0 +1,46 @@
+package com.SpringProj.todo.Controllers;
+
+import com.SpringProj.todo.DTOs.AuthResponseDto;
+import com.SpringProj.todo.DTOs.LoginDto;
+import com.SpringProj.todo.DTOs.RegisterDto;
+import com.SpringProj.todo.Services.Auth.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> Register(@RequestBody RegisterDto registerDto) {
+
+        AuthResponseDto authResponse = authService.register(registerDto);
+
+        if(!authResponse.isAuthenticated())
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(authResponse);
+
+        return ResponseEntity.status(HttpStatus.OK).body(authResponse);
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> Login(@RequestBody LoginDto loginDto) {
+
+        AuthResponseDto authResponse = authService.login(loginDto);
+
+        if(!authResponse.isAuthenticated())
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(authResponse);
+
+        return ResponseEntity.status(HttpStatus.OK).body(authResponse);
+    }
+
+
+}

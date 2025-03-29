d4e127207b5eedfeabe3c986dd0916a95c06ee37
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-03-29 22:15:33 +0200

    Adding SubTasks Repo

---
+++ src/main/java/com/SpringProj/todo/Repository/SubTaskRepository.java
@@ -0,0 +1,13 @@
+package com.SpringProj.todo.Repository;
+
+import com.SpringProj.todo.Model.SubTask;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SubTaskRepository extends JpaRepository<SubTask, Long> {
+
+    Optional<List<SubTask>> findByTask_Id(Long id);
+
+}

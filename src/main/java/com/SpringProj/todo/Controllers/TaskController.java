b964b6dc1d51c289633e6c5c231ee15b973f0d99
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-03-30 05:41:40 +0200

    full actions signatures for TaskController

--- src/main/java/com/SpringProj/todo/Controllers/TaskController.java
+++ src/main/java/com/SpringProj/todo/Controllers/TaskController.java
@@ -1,10 +1,81 @@
 package com.SpringProj.todo.Controllers;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.SpringProj.todo.DTOs.TaskDTOs.TaskCreateDto;
+import com.SpringProj.todo.DTOs.TaskDTOs.TaskReadDto;
+import com.SpringProj.todo.DTOs.TaskDTOs.TaskUpdateDto;
+import com.SpringProj.todo.Model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("api/category")
+@RequestMapping("api/task")
 public class TaskController {
+
+
+    @GetMapping("/get-all")
+    public ResponseEntity<List<TaskReadDto>> getAllTasks(@AuthenticationPrincipal User user,
+                                                         @RequestParam(required = false) String category,
+                                                         @RequestParam(required = false) String search,
+                                                         @RequestParam(required = false) String sort,
+                                                         @RequestParam(defaultValue = "1") int page
+                                                         )
+    {
+        return null;
+    }
+
+    @GetMapping("/getById/{id}")
+    public ResponseEntity<TaskReadDto> getById(@AuthenticationPrincipal User user, @PathVariable Long id)
+    {
+        return null;
+    }
+
+
+    @GetMapping("/getByCategoryId")
+    public ResponseEntity<List<TaskReadDto>> getByCategoryId(@AuthenticationPrincipal User user,
+                                                             @RequestParam Long categoryId)
+    {
+        return null;
+    }
+
+    @GetMapping("/getByStatus")
+    public ResponseEntity<List<TaskReadDto>> getByStatus(@AuthenticationPrincipal User user, String status)
+    {
+        return null;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<TaskReadDto> addTask(@RequestBody TaskCreateDto taskCreateDto,
+                                           @AuthenticationPrincipal User user)
+    {
+        return null;
+    }
+
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<TaskReadDto> updateTask(@RequestParam Long id,
+                                                  @RequestBody TaskUpdateDto taskUpdateDto,
+                                                  @AuthenticationPrincipal User user)
+    {
+        return null;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteTask(@PathVariable Long id)
+    {
+        return null;
+    }
+
+
+
+    @PutMapping("/changeStatus/{id}")
+    public ResponseEntity<TaskReadDto> changeTaskStatus(@PathVariable Long id, @RequestParam String status
+            ,@AuthenticationPrincipal User user)
+    {
+        return null;
+    }
+
 }

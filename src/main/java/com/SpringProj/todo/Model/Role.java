0e9d41df5049766280a5641545f8bd23a791eccc
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-04-12 12:02:34 +0200

    using Granted Authority to implement role

--- src/main/java/com/SpringProj/todo/Model/Role.java
+++ src/main/java/com/SpringProj/todo/Model/Role.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.List;
 
@@ -12,7 +13,7 @@ import java.util.List;
 @Table(name = "role")
 @Data
 
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,4 +24,10 @@ public class Role {
 
     @ManyToMany(mappedBy = "roles")
     private List<User> users;
+
+    @Override
+    public String getAuthority() {
+        return role;
+    }
+
 }

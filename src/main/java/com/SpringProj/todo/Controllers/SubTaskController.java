e3fd3d3a2f01ab6d4782aa44e3ae8cc564b02604
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-04-15 10:25:53 +0200

    Finishing SubTaskController

--- src/main/java/com/SpringProj/todo/Controllers/SubTaskController.java
+++ src/main/java/com/SpringProj/todo/Controllers/SubTaskController.java
@@ -6,55 +6,199 @@ import com.SpringProj.todo.DTOs.SubTaskDTOs.SubTaskReadDto;
 import com.SpringProj.todo.DTOs.SubTaskDTOs.SubTaskUpdateDto;
 import com.SpringProj.todo.DTOs.TaskDTOs.TaskReadDto;
 import com.SpringProj.todo.DTOs.TaskDTOs.TaskUpdateDto;
+import com.SpringProj.todo.Model.SubTask;
 import com.SpringProj.todo.Model.User;
+import com.SpringProj.todo.Responses.ApiResponse;
+import com.SpringProj.todo.Services.Subtask.SubtaskService;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/subtask")
+@RequiredArgsConstructor
 public class SubTaskController {
 
+    private final SubtaskService subtaskService;
+
     @GetMapping("/getById/{id}")
-    public ResponseEntity<SubTaskReadDto> getById(@AuthenticationPrincipal User user, @PathVariable Long id)
+    public ResponseEntity<ApiResponse<SubTaskReadDto>> getById(@AuthenticationPrincipal User user, @PathVariable Long id)
     {
-        return null;
+        ApiResponse<SubTaskReadDto> res = new ApiResponse<>();
+
+        try {
+
+            SubTask subTask = subtaskService.getById(id, user);
+
+            res.setData(new SubTaskReadDto(subTask));
+            res.setSuccess(true);
+
+            return ResponseEntity.ok(res);
+
+        }
+        catch (AccessDeniedException e) {
+
+            res.setSuccess(false);
+            res.setMessage(e.getMessage());
+
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(res);
+
+        }
+
+        catch (EntityNotFoundException e) {
+
+            res.setSuccess(false);
+            res.setMessage(e.getMessage());
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(res);
+        }
     }
 
     @GetMapping("/getByTaskId")
-    public ResponseEntity<SubTaskReadDto> getByTaskId(@AuthenticationPrincipal User user, @RequestParam Long id)
+    public ResponseEntity<ApiResponse<SubTaskReadDto>> getByTaskId(@AuthenticationPrincipal User user, @RequestParam Long taskId)
     {
-        return null;
+        ApiResponse<SubTaskReadDto> res = new ApiResponse<>();
+
+        try {
+
+            SubTask subTask = subtaskService.getById(taskId, user);
+            res.setData(new SubTaskReadDto(subTask));
+            res.setSuccess(true);
+
+            return ResponseEntity.ok(res);
+
+        }
+        catch(EntityNotFoundException e) {
+
+            res.setSuccess(false);
+            res.setMessage(e.getMessage());
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(res);
+        }
+        catch (AccessDeniedException e) {
+
+            res.setSuccess(false);
+            res.setMessage(e.getMessage());
+
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(res);
+        }
+
+
     }
 
     @PostMapping("/add")
-    public ResponseEntity<SubTaskReadDto> addSubTask(@AuthenticationPrincipal User user,
-                                                     @RequestBody SubTaskCreateDto subTaskCreateDto)
+    public ResponseEntity<ApiResponse<SubTaskReadDto>> addSubTask(@RequestBody SubTaskCreateDto subTaskCreateDto)
     {
-        return null;
+        ApiResponse<SubTaskReadDto> res = new ApiResponse<>();
+
+        try {
+
+            subtaskService.addSubTask(subTaskCreateDto);
+
+            res.setSuccess(true);
+            return ResponseEntity.status(HttpStatus.CREATED).body(res);
+
+        }
+        catch(EntityNotFoundException e) {
+
+            res.setSuccess(false);
+            res.setMessage(e.getMessage());
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(res);
+        }
+        catch (AccessDeniedException e) {
+
+            res.setSuccess(false);
+            res.setMessage(e.getMessage());
+
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(res);
+        }
     }
 
     @PutMapping("/changeStatus/{id}")
-    public ResponseEntity<SubTaskReadDto> changeSubTaskStatus(@PathVariable Long id, @RequestParam String status
+    public ResponseEntity<ApiResponse<SubTaskReadDto>> changeSubTaskStatus(@PathVariable Long id, @RequestParam String status
             ,@AuthenticationPrincipal User user)
     {
-        return null;
+        ApiResponse<SubTaskReadDto> res = new ApiResponse<>();
+
+        try {
+
+            SubTask subTask = subtaskService.ChangeStatus(id, status, user);
+            res.setData(new SubTaskReadDto(subTask));
+            res.setSuccess(true);
+
+            return ResponseEntity.ok(res);
+        }
+        catch (EntityNotFoundException e) {
+
+            res.setSuccess(false);
+            res.setMessage(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(res);
+        }
+        catch (AccessDeniedException e) {
+            res.setSuccess(false);
+            res.setMessage(e.getMessage());
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(res);
+        }
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<SubTaskReadDto> updateTask(@RequestParam Long id,
+    public ResponseEntity<ApiResponse<SubTaskReadDto>> updateTask(@RequestParam Long id,
                                                   @RequestBody SubTaskUpdateDto subTaskUpdateDto,
                                                   @AuthenticationPrincipal User user)
     {
-        return null;
+        ApiResponse<SubTaskReadDto> res = new ApiResponse<>();
+
+        try{
+
+            SubTask subtask = subtaskService.updateSubTask(id, subTaskUpdateDto, user);
+
+            res.setData(new SubTaskReadDto(subtask));
+            res.setSuccess(true);
+
+            return ResponseEntity.ok(res);
+        }
+        catch(EntityNotFoundException e) {
+            res.setSuccess(false);
+            res.setMessage(e.getMessage());
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(res);
+        }
+        catch (AccessDeniedException e) {
+            res.setSuccess(false);
+            res.setMessage(e.getMessage());
+
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(res);
+        }
     }
 
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> deleteTask(@PathVariable Long id, @AuthenticationPrincipal User user)
+      public ResponseEntity<ApiResponse<Void>> deleteTask(@PathVariable Long id, @AuthenticationPrincipal User user)
     {
-        return null;
-    }
+        ApiResponse<Void> res = new ApiResponse<>();
 
+        try {
+            subtaskService.deleteSubTask(id, user);
+            res.setSuccess(true);
+            return ResponseEntity.ok(res);
+        }
+        catch (EntityNotFoundException e) {
+
+            res.setSuccess(false);
+            res.setMessage(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(res);
+        }
+        catch (AccessDeniedException e) {
+            res.setSuccess(false);
+            res.setMessage(e.getMessage());
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(res);
+
+        }
+    }
 
 }

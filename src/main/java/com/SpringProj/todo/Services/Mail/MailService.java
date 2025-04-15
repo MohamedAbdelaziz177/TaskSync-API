b1ec60963ee955712592352402a42f7956b6bf24
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-04-15 15:44:22 +0200

    Adding html mails sender

--- src/main/java/com/SpringProj/todo/Services/Mail/MailService.java
+++ src/main/java/com/SpringProj/todo/Services/Mail/MailService.java
@@ -1,6 +1,9 @@
 package com.SpringProj.todo.Services.Mail;
 
+import jakarta.mail.MessagingException;
+
 public interface MailService {
 
     void sendSimpleMail(String to, String subject, String content);
+    void sendHtmlMail(String to, String subject, String content) throws MessagingException;
 }
--- src/main/java/com/SpringProj/todo/Services/Mail/MailServiceImpl.java
+++ src/main/java/com/SpringProj/todo/Services/Mail/MailServiceImpl.java
@@ -1,5 +1,9 @@
 package com.SpringProj.todo.Services.Mail;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.AddressException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -19,6 +23,20 @@ public class MailServiceImpl implements MailService {
         message.setTo(to);
         message.setSubject(subject);
         message.setText(content);
+
+        mailSender.send(message);
+
+    }
+
+
+    public void sendHtmlMail(String to, String subject, String content) throws MessagingException {
+
+        MimeMessage message = mailSender.createMimeMessage();
+
+        message.setFrom(new InternetAddress("mohamecabdelaziz66@gmail.com"));
+        message.setSubject(subject);
+        message.setContent(content, "text/html;charset=UTF-8");
+        message.setRecipients(MimeMessage.RecipientType.TO, "mohamecabdelaziz66@gmail.com");
 
         mailSender.send(message);
 

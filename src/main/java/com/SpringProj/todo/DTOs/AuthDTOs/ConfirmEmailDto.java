1b317e51cffaa45dad5312735ffbae6c637d27da
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-04-15 16:35:51 +0200

    Adding Reset password & confirm email functionality

---
+++ src/main/java/com/SpringProj/todo/DTOs/AuthDTOs/ConfirmEmailDto.java
@@ -0,0 +1,18 @@
+package com.SpringProj.todo.DTOs.AuthDTOs;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConfirmEmailDto {
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private Long otp;
+}

661fe06f00996c6be33ac30eff6cf65a0745149b
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-04-15 12:52:28 +0200

    Adding success boolean value

--- src/main/java/com/SpringProj/todo/Responses/TokenResponse.java
+++ src/main/java/com/SpringProj/todo/Responses/TokenResponse.java
@@ -1,14 +1,18 @@
 package com.SpringProj.todo.Responses;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class TokenResponse {
 
     private String accessToken;
     private String refreshToken;
+
+    private boolean success;
 }

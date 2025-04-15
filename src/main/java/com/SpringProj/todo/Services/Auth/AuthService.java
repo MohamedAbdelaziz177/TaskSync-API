4c646a1b679e495ceab4e0a09a73217b1c296d61
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-04-15 16:35:41 +0200

    Adding Reset password & confirm email functionality

--- src/main/java/com/SpringProj/todo/Services/Auth/AuthService.java
+++ src/main/java/com/SpringProj/todo/Services/Auth/AuthService.java
@@ -1,5 +1,8 @@
 package com.SpringProj.todo.Services.Auth;
 
+import com.SpringProj.todo.DTOs.AuthDTOs.ConfirmEmailDto;
+import com.SpringProj.todo.DTOs.AuthDTOs.ResetPasswordDto;
+import com.SpringProj.todo.Model.User;
 import com.SpringProj.todo.Responses.AuthResponse;
 import com.SpringProj.todo.DTOs.AuthDTOs.LoginDto;
 import com.SpringProj.todo.DTOs.AuthDTOs.RegisterDto;
@@ -10,4 +13,7 @@ public interface AuthService {
     AuthResponse login(LoginDto loginDto);
     AuthResponse register(RegisterDto registerDto);
     TokenResponse refreshToken(String refreshToken);
+    void sendOtpToUser(User user);
+    Boolean confirmEmail(ConfirmEmailDto confirmEmailDto);
+    Boolean resetPassword(ResetPasswordDto resetPasswordDto);
 }
--- src/main/java/com/SpringProj/todo/Services/Auth/AuthServiceImpl.java
+++ src/main/java/com/SpringProj/todo/Services/Auth/AuthServiceImpl.java
@@ -1,5 +1,7 @@
 package com.SpringProj.todo.Services.Auth;
 
+import com.SpringProj.todo.DTOs.AuthDTOs.ConfirmEmailDto;
+import com.SpringProj.todo.DTOs.AuthDTOs.ResetPasswordDto;
 import com.SpringProj.todo.Responses.AuthResponse;
 import com.SpringProj.todo.DTOs.AuthDTOs.LoginDto;
 import com.SpringProj.todo.DTOs.AuthDTOs.RegisterDto;
@@ -7,6 +9,7 @@ import com.SpringProj.todo.Model.User;
 import com.SpringProj.todo.Repository.UserRepository;
 import com.SpringProj.todo.Responses.TokenResponse;
 import com.SpringProj.todo.Services.Jwt.JwtService;
+import com.SpringProj.todo.Services.Mail.MailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,16 +21,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.NoSuchElementException;
-import java.util.Optional;
+import java.util.*;
 
 @RequiredArgsConstructor
 @Service
 public class AuthServiceImpl implements AuthService {
 
     private final JwtService jwtService;
+    private final MailService mailService;
     private final AuthenticationManager authenticationManager;
     private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
@@ -93,6 +94,59 @@ public class AuthServiceImpl implements AuthService {
 
             return tokenResponse;
 
+    }
+
+    private Long generateOtp()
+    {
+        return new Random().nextLong(100000, 199999);
+    }
+
+    public void sendOtpToUser(User user){
+
+        Long otp = generateOtp();
+        mailService.sendSimpleMail(user.getEmail(), "Confirmation Code", otp.toString());
+        user.setCode(otp);
+        user.setCodeExpiryDate(new Date(System.currentTimeMillis() + 86400000));
+        user.setVerified(false);
+    }
+
+    public Boolean confirmEmail(ConfirmEmailDto confirmEmailDto)
+    {
+        User user = userRepository.findByEmail(confirmEmailDto.getEmail()).orElseThrow(() ->
+                new NoSuchElementException("No such user found"));
+
+        if(!user.getCode().equals(confirmEmailDto.getOtp()) ||
+                user.getCodeExpiryDate().getTime() < System.currentTimeMillis())
+            return false;
+
+        user.setVerified(true);
+        user.setCode(null);
+
+        userRepository.save(user);
+
+        return true;
+    }
+
+    public Boolean resetPassword(ResetPasswordDto resetPasswordDto)
+    {
+        User user = userRepository.findByEmail(resetPasswordDto.getEmail()).orElseThrow(() ->
+                new NoSuchElementException("No such user found"));
+
+        if(!user.getCode().equals(resetPasswordDto.getOtp()) ||
+                user.getCodeExpiryDate().getTime() < System.currentTimeMillis())
+        {
+            return false;
+        }
+
+        if(!resetPasswordDto.getConfirmPassword().equals(resetPasswordDto.getPassword()))
+            return false;
+
+        user.setPassword(passwordEncoder.encode(resetPasswordDto.getPassword()));
+        user.setCode(null);
+
+        userRepository.save(user);
+
+        return true;
     }
 
 

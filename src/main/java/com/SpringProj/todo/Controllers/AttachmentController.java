caa2a2f5cc46adc068e807471977d105fd855091
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-04-16 11:04:20 +0200

    intializing CategoryController / AttachmentController

---
+++ src/main/java/com/SpringProj/todo/Controllers/AttachmentController.java
@@ -0,0 +1,14 @@
+package com.SpringProj.todo.Controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/attachment")
+public class AttachmentController {
+
+    // get by TaskId - get by subtaskId - assign to a task - assign to sub task - delete
+    // ربنا يسهل
+}
--- src/main/java/com/SpringProj/todo/Controllers/CategoryController.java
+++ src/main/java/com/SpringProj/todo/Controllers/CategoryController.java
@@ -1,11 +1,41 @@
 package com.SpringProj.todo.Controllers;
 
+import com.SpringProj.todo.Model.Category;
+import com.SpringProj.todo.Model.User;
+import com.SpringProj.todo.Responses.ApiResponse;
+import lombok.Data;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/category")
 public class CategoryController {
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse<String>> addCategory(String category)
+    {return null;}
+
+    @PostMapping("/assign-to-user")
+    public ResponseEntity<ApiResponse<String>> addUserCategory(@AuthenticationPrincipal User user, String category)
+    {return null;}
+
+    @GetMapping("/get-by-user")
+    public ResponseEntity<ApiResponse<List<String>>> getUserCategories(@AuthenticationPrincipal User user)
+    {return null;}
+
+    @GetMapping("/get-all") // only for admins / analysis purposes for eg
+    public ResponseEntity<ApiResponse<List<String>>> getAllCategories()
+    {return null;}
+
+    @GetMapping("/get-tasks")
+    public ResponseEntity<ApiResponse<List<String>>> getAllTasksByCategory(Long id)
+    {return null;}
+
+    @DeleteMapping("delete-user-category")
+    public ResponseEntity<ApiResponse<String>> deleteUserCategory(@AuthenticationPrincipal User user, Long id)
+    {return null;}
 
 }

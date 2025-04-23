a0227e494fddf866dcdc8ba0ab0849fef6b39eed
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2025-04-23 13:54:57 +0200

    Attachment controller added

--- src/main/java/com/SpringProj/todo/Model/Attachment.java
+++ src/main/java/com/SpringProj/todo/Model/Attachment.java
@@ -4,9 +4,11 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @MappedSuperclass
+@Data
 public class Attachment {
 
     @NotBlank(message = "fileName cannot be empty")
